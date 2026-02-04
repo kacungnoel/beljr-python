51ca9d196a1885cd985333008caf4065522afcf5
kacungnoel <noelff2112@gmail.com>
2026-02-04 19:22:32 +0800

    beljr h7

---
+++ bljr7.py
@@ -0,0 +1,18 @@
+# belajar 7 - list dan perulangan lanjutan di pyhton
+# funsi dalam list seperti append, insert, remove,pop,clear,index,count,sort,reverse,copy
+
+#contoh penggunaan append 
+buah_buahan = ["apel", "mangga","anggur"]
+print ("list awal :",buah_buahan)
+
+print ("============Append===============")
+
+#menambahkan elemen baru ke dalam list 
+buah_buahan. append ('kiwi')
+print ("setelah append kiwi:",buah_buahan)
+
+print ("============Insert=================")
+#menyisipkan elemen pada indeks tertentu
+buah_buahan.insert(1,'pisang')
+print ("setelah insert pisang pada indeks 1:, buah_buahan")
+
